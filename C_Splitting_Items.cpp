8310a9dc84dca805ed29c0eccad671ce5f14d424
alamgir65 <alamgirhossaindrubo@gmail.com>
2024-08-21 12:47:35 +0600

    new added

---
+++ C_Splitting_Items.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define print(flag) (flag)? cout<<"YES\n" : cout<<"NO\n";
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define dqii deque<int> 
+#define dqll deque<ll> 
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+void solve(){
+    ll n,k; cin>>n>>k;
+    vll a(n);
+    ll mx=imin;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        mx=max(mx,a[i]);
+    }
+    sort(all(a));
+    reverse(all(a));
+    ll ans=0;
+    if(n%2==1){
+        ans += a[n-1];
+        n--;
+    }
+    for(int i=0;i<n;i+=2){
+        int x=a[i]-a[i+1];
+        if(k >= x){
+            k -= x;
+        }else{
+            x -= k;
+            k=0;
+            ans += x;
+        }
+    }
+    out(ans)
+}
+love{
+    Alamgir
+    int t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        // cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;    
+}
--- tmp.cpp
+++ tmp.cpp
@@ -1,18 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-    int n,x; cin>>n>>x;
-    for(int i=0;i<n;i++){
-        cout<<i<<" ";
-    }
-    cout<<endl;
-    vector<int> v;
-    for(int i=0;i<x;i++){
-        int s; cin>>s;
-        v.push_back(s);
-    }
-    for(auto s:v) cout<<s<<" ";
-    cout<<endl;
+    
 }
 int main(){
     int t; cin>>t;

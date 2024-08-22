66cf5f0a6ea2d8d20c575b44817e1e29e3ddefc1
alamgir65 <alamgirhossaindrubo@gmail.com>
2024-08-22 23:01:44 +0600

    done

---
+++ A_Submission_Bait.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+#define cyes cout<<"Yes"<<endl;
+#define cno cout<<"No"<<endl;
+#define print(flag) (flag)? cout<<"YES\n" : cout<<"NO\n";
+#define all(x) x.begin(), x.end()
+#define nl cout<<endl;
+#define endl '\n'
+#define love int main()
+#define vtr vector
+#define pii pair<int,int>
+#define out(x) cout << x << '\n';
+#define out2(x,y) cout << x << " " << y << '\n';
+#define vii vector<int> 
+#define vll vector<ll>
+#define dqii deque<int> 
+#define dqll deque<ll> 
+#define imax INT_MAX
+#define imin INT_MIN
+#define pb push_back
+#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+void solve(){
+    int n; cin>>n;
+    vii a(n);
+    map<int,int> mp;
+    int mx=imin;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        mp[a[i]]++;
+        mx=max(mx,a[i]);
+    }
+    for(auto [x,y]:mp){
+        if(y%2==1){
+            yes
+            return;
+        }
+    }
+    no
+}
+love{
+    Alamgir
+    int t=1;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        // cout<<"Case "<<i<<": ";
+        solve();
+    }
+    return 0;    
+}

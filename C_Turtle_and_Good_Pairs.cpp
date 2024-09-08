e0b9bb589a7f6ae4f891a1244e0732162fdb26ca
alamgir65 <alamgirhossaindrubo@gmail.com>
2024-09-09 00:03:32 +0600

    hello

--- C_Turtle_and_Good_Pairs.cpp
+++ C_Turtle_and_Good_Pairs.cpp
@@ -1,42 +1,37 @@
 #include<bits/stdc++.h>
-#define ll long long
-#define yes cout<<"YES"<<endl;
-#define no cout<<"NO"<<endl;
-#define cyes cout<<"Yes"<<endl;
-#define cno cout<<"No"<<endl;
-#define print(flag) (flag)? cout<<"YES\n" : cout<<"NO\n";
-#define all(x) x.begin(), x.end()
-#define nl cout<<endl;
-#define endl '\n'
-#define love int main()
-#define vtr vector
-#define pii pair<int,int>
-#define out(x) cout << x << '\n';
-#define out2(x,y) cout << x << " " << y << '\n';
-#define vii vector<int> 
-#define vll vector<ll>
-#define dqii deque<int> 
-#define dqll deque<ll> 
-#define imax INT_MAX
-#define imin INT_MIN
-#define pb push_back
-#define Alamgir ios_base::sync_with_stdio(false), cin.tie(0),cout.tie(0);
 using namespace std;
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
- 
-template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-void solve(){
-    
-}
-love{
-    Alamgir
-    int t=1;
-    cin>>t;
-    for(int i=1;i<=t;i++){
-        // cout<<"Case "<<i<<": ";
-        solve();
-    }
-    return 0;    
+const int sz = 1e5 + 5, part = 300;
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    int t; 
+    cin >> t;
+    while(t--) {
+    	int n; cin >> n;
+    	string s; cin >> s;
+    	std::vector<int> v(26);
+    	for(char &c:s) v[c - 'a']++;
+    	int mxid = 0;
+    	for(int i = 1; i < 26; i++) {
+    		if(v[i] > v[mxid]) mxid = i;
+    	}
+    	string ans = "";
+    	ans += char('a' + mxid);
+    	v[mxid]--;
+    	while(ans.size() < n) {
+    		int hi = -1;
+    		int last = ans.back() - 'a';
+    		for(int i = 0; i < 26; i++) {
+    			if(v[i] == 0) continue;
+    			if(i != last && (hi == -1 || v[i] > v[hi]))
+    				hi = i;
+    		}
+    		if(hi == -1) hi = last;
+    		ans += char('a' + hi);
+    		v[hi]--;
+    	}
+    	// sort(s.begin(), s.end());
+    	cout << ans << "\n";
+	}
+    return 0;
 }
